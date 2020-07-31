6af4aabc8c5c778761e40d61f79113b6d9584262
Pavan <pavan.sagar@outlook.com>
2020-07-31 16:09:25 +0530

    Update index.js
    Removed default Nextjs message

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,7 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I am Pavan who is learning web development to become a Full Stack developer !</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        
       </section>
     
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
